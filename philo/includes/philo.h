c657d0b51dc39be18aedcae581d3917094f20978
Loup Thevenin <loup@macbook-pro-de-loup.home>
2024-12-29 19:46:27 +0100

    Start philo

---
+++ philo/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltheveni <ltheveni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/29 19:39:40 by ltheveni          #+#    #+#             */
+/*   Updated: 2024/12/29 19:42:06 by ltheveni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
